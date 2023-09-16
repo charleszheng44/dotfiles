5aeaa510a26853dbd2dc36187d6435ce8aac438a
Charles Zheng <charleszheng44@gmail.com>
2023-09-15 19:36:10 -0700

    add nightfox alacritty

--- setup.sh
+++ setup.sh
@@ -4,7 +4,15 @@ set -e
 
 CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# setup alacritty
+[ -d ~/.config/alacritty ] && rm -rf ~/.config/alacritty
+[ -L ~/.config/alacritty ] && rm ~/.config/alacritty
+[ -d ~/.config ] || mkdir ~/.config
+ln -s $CONFIG_DIR/alacritty ~/.config/alacritty
+
 # setup zshrc
+[ -d ~/.oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 [ -f ~/.zshrc ] && rm ~/.zshrc
 [ -L ~/.zshrc ] && rm ~/.zshrc
 ln -s $CONFIG_DIR/zshrc ~/.zshrc
@@ -26,3 +34,6 @@ ln -s $CONFIG_DIR/tmux ~/.tmux
 mkdir -p ~/.config && ln -s $CONFIG_DIR/starship.toml ~/.config/starship.toml
 
 # setup neovim
+[ -d ~/.config/nvim ] && rm -rf ~/.config/nvim
+[ -L ~/.config/nvim ] && rm ~/.config/nvim
+ln -s $CONFIG_DIR/nvim ~/.config/nvim
